97563cbf807e1372a76a3b3addd448c46f6199b8
Oskar André <93657491+oskarandre@users.noreply.github.com>
2024-11-13 15:34:55 +0100

    Kotlin and xml done

--- Kotlinxml/app/src/main/java/com/example/kotlinxml/MainActivity.kt
+++ Kotlinxml/app/src/main/java/com/example/kotlinxml/MainActivity.kt
@@ -1,12 +1,16 @@
 package com.example.kotlinxml
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btn1: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +20,8 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        btn1 = findViewById(R.id.button1)
+
+
     }
 }
